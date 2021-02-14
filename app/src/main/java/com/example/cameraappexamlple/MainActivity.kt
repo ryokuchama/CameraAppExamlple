d51d0786b00fee4ce7c493bf5241d19a461dc140
tarou <r.madrid.pg@gmail.com>
2021-02-14 22:57:16 +0900

    0214

--- app/src/main/java/com/example/cameraappexamlple/MainActivity.kt
+++ app/src/main/java/com/example/cameraappexamlple/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.cameraappexamlple
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.ImageFormat
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraManager
@@ -18,11 +19,10 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import java.lang.Exception
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,12 +44,8 @@ class MainActivity : AppCompatActivity() {
                 type = "image/*"
             }
 
-            startActivityForResult(intent, 42)
+            startActivityForResult(intent, READ_REQUEST_CODE)
         }
-
-//        companion object {
-//            private const val READ_REQUEST_CODE: Int = 42
-//        }
 
         comparativeButton.setOnClickListener {
             val intent: Intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
@@ -57,7 +53,7 @@ class MainActivity : AppCompatActivity() {
                 type = "image/*"
             }
 
-            startActivityForResult(intent, 42)
+            startActivityForResult(intent, READ_REQUEST_CODE)
         }
 
         find.setOnClickListener {
@@ -68,5 +64,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != RESULT_OK) {
+            return
+        }
+        when (requestCode) {
+            READ_REQUEST_CODE -> {
+                try {
+                    data?.also { uri ->
+                        val inputStream = contentResolver?.openInputStream(uri)
+                        val image = BitmapFactory.decodeStream(inputStream)
+
+                    }
+                }
+                catch (e: Exception) {
+
+                }
+            }
+        }
+    }
+
+    // クラス内に生成されるシングルトン
+    // シングルトン -> 状態を持たないこと、抽象クラスまたはインターフェースを実装していること
+    companion object {
+        const val READ_REQUEST_CODE: Int = 42
     }
 }
