5a798de3353b4b6d48685d15ade76efac543fc28
tarou <r.madrid.pg@gmail.com>
2021-02-19 00:52:10 +0900

    0219

--- app/src/main/java/com/example/cameraappexamlple/MainActivity.kt
+++ app/src/main/java/com/example/cameraappexamlple/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cameraappexamlple
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.graphics.ImageFormat
 import android.graphics.SurfaceTexture
@@ -19,14 +20,17 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.lang.Exception
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
-    var correct: ImageView? = null
-    var comparative: ImageView? = null
+    // lateinit -> delay initialization until onCreate is called
+    lateinit var correct: ImageView
+    lateinit var comparative: ImageView
 
     var mode: Int = 0
 
@@ -35,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        correct= findViewById(R.id.src)
+        correct = findViewById(R.id.src)
         comparative = findViewById(R.id.target)
         val correctButton: Button = findViewById(R.id.correctImageButton)
         val comparativeButton: Button = findViewById(R.id.comparativeImageButton)
@@ -74,9 +78,39 @@ class MainActivity : AppCompatActivity() {
                 if (resultCode == Activity.RESULT_OK && requestCode == READ_REQUEST_CODE) {
 
                     when {
-                        mode == 0 -> correct
+                        mode == 0 -> correct.setImageURI(data?.data)
+                        mode == 1 -> comparative.setImageURI(data?.data)
                     }
                 }
+            }
+        }
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+                this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        when(requestCode) {
+            1000 -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(applicationContext, "Request is not permitted", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    Toast.makeText(applicationContext, "Request permitted", Toast.LENGTH_SHORT).show()
+                }
+                return
             }
         }
     }
